9fa53674359aa4e189a53bbb749b54bae893cac5
Abrar rizvi <abrar8949@gmail.com>
2024-04-07 07:37:29 -0700

    uploading file

---
+++ index.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+//imported inquirer from node_module folder
+import inquirer from "inquirer";
+//declaring a variable to store currencies and making usd as base currency
+const currency = {
+    USD: 1, // base currency
+    GBP: 0.79,
+    INR: 83.30,
+    PKR: 277.54,
+    UAE: 3.67,
+    SR: 3.75,
+    CAD: 1.36,
+};
+//getting input from user 
+const ans = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Convert currency from",
+        type: "list",
+        choices: ["USD", "INR", "PKR", "UAE", "SR", "CAD", "GBP"]
+    },
+    {
+        name: "to",
+        message: "To",
+        type: "list",
+        choices: ["USD", "INR", "PKR", "UAE", "SR", "CAD", "GBP"]
+    },
+    {
+        name: "amount",
+        message: "Enter amount",
+        type: "number",
+    },
+]);
+let fromAmount = currency[ans.from]; // it gets exchange rate of currency 
+let toAmount = currency[ans.to]; // it gets exchange rate of currency 
+let amount = ans.amount; // it gets amount which user give in input
+let baseAmount = amount / fromAmount; // conversion into base amount dollar
+let ConvertedAmount = toAmount * baseAmount; // base amount is converted to desired amount in which we want conversion
+// removing  decimal from  converted amount.
+let roundOff = Math.floor(ConvertedAmount);
+console.log(roundOff);
